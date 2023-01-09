f4a180a4360c9972f379f89da899b35dd74320eb
thatsongsucks <andrew.tisdall@gmail.com>
2023-01-08 22:56:51 -0800

    rework docusign success page

--- src/components/onboarding/DocusignSuccess.js
+++ src/components/onboarding/DocusignSuccess.js
@@ -1,42 +1,69 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Spinner from 'react-activity/dist/Spinner';
+import 'react-activity/dist/Spinner.css';
 
 import server from '../../actions/api';
 
 const DocusignSuccess = ({ restaurant }) => {
-  const [result, setResult] = useState('');
-  const [envelopeId, setEnvelopeId] = useState('');
+  const [fileCount, setFileCount] = useState(0);
+  const [success, setSuccess] = useState(null);
 
   useEffect(() => {
-    const envelopeIdParam = window.location.search.split('?')[1];
-    // const event = searchParams[2].replace('event=', '');
-    // if (event !== 'signing_complete') {
-    //   setResult('Fail');
-    // } else {
-    setResult('Succeed');
-    setEnvelopeId(envelopeIdParam.replace('envelopeId=', ''));
-    // }
-  }, []);
-
-  useEffect(() => {
-    if (result === 'Succeed') {
+    const params = window.location.search.split('?')[1].split('&');
+    const envelopeId = params[0].replace('envelopeId=', '');
+    const event = params[1].replace('event=', '');
+    if (event === 'signing_complete') {
+      setSuccess(true);
       const updateSalesforce = async () => {
-        const filesAdded = await server.post('/docusign/getDoc', {
+        const { data } = await server.post('/docusign/getDoc', {
           restaurantId: restaurant.id,
           envelopeId,
         });
-        console.log('Files added: ' + filesAdded);
+        setFileCount(data.filesAdded);
       };
       updateSalesforce();
+    } else {
+      setSuccess(false);
     }
-  }, [result]);
+  }, [restaurant.id]);
+
+  const renderFileCount = () => {
+    if (fileCount) {
+      return (
+        <div>
+          <p>{fileCount} document(s) uploaded.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Uploading signed documents...</p>
+          <Spinner size={20} color="black" />
+        </div>
+      );
+    }
+  };
+
+  const renderSuccess = () => {
+    return (
+      <>
+        <h2>Docs Signed Successfully</h2>
+        {renderFileCount()}
+      </>
+    );
+  };
+
+  const renderFailure = () => {
+    return <h2>Signing Failed</h2>;
+  };
 
   return (
     <div>
-      <div>
-        <p>You {result}</p>
-      </div>
+      {success && renderSuccess()}
+      {success === false && renderFailure()}
+      {success === null && <Spinner size={20} color="black" />}
       <Link to="../..">
         <button>Go back to Onboarding Home</button>
       </Link>
