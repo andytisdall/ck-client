801211b230348b1b0c1361ad34e2b50aa5ed213a
Andrew Tisdall <andrewtisdall@Andrews-MacBook-Pro.local>
2023-03-28 22:18:08 -0700

    add loading state to google login

--- src/components/auth/GoogleSignIn.js
+++ src/components/auth/GoogleSignIn.js
@@ -1,10 +1,24 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { connect } from 'react-redux';
 
+import useLoading from '../../hooks/useLoading';
+import Loading from '../reusable/Loading';
 import * as actions from '../../actions';
 
 const GoogleSignIn = ({ setError, googleSignIn }) => {
-  return <GoogleLogin onSuccess={googleSignIn} onError={setError} />;
+  const [loading, setLoading] = useLoading();
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <GoogleLogin
+      onSuccess={() => {
+        setLoading(true);
+        googleSignIn();
+      }}
+      onError={setError}
+    />
+  );
 };
 
 export default connect(null, actions)(GoogleSignIn);
