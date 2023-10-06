25e34dd38d1b2cae58d454df17395fef58622efb
Andrew Tisdall <andrewtisdall@Andrews-MacBook-Pro.local>
2023-10-06 15:20:14 -0700

    new home chef orientation materials

--- src/components/homeChef/onboarding/OrientationSlides.js
+++ src/components/homeChef/onboarding/OrientationSlides.js
@@ -5,7 +5,7 @@ import './Orientation.css';
 const OrientationSlides = () => {
   const renderSlides = () => {
     const images = [];
-    for (let i = 1; i < 23; i++) {
+    for (let i = 1; i <= 20; i++) {
       images.push(
         <img
           className="orientation-slides"
