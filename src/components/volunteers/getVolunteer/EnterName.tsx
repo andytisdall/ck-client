0802a60c2b590778bd733d05d7ba504a4a45f7e7
Andrew Tisdall <andrewtisdall@Andrews-MacBook-Pro.local>
2023-11-17 14:19:58 -0800

    header and volunteers styling for phone

--- src/components/volunteers/getVolunteer/EnterName.tsx
+++ src/components/volunteers/getVolunteer/EnterName.tsx
@@ -15,23 +15,27 @@ const EnterName = ({
         Please enter your name for our records (in the future you'll only need
         to enter your email):
       </h4>
-      <label htmlFor="firstName">First Name:</label>
-      <input
-        autoFocus
-        type="text"
-        id="firstName"
-        required
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <label htmlFor="lastName">Last Name:</label>
-      <input
-        type="text"
-        id="lastName"
-        required
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
+      <div className="volunteers-signin-field-item">
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          autoFocus
+          type="text"
+          id="firstName"
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="volunteers-signin-field-item">
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
